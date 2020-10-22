4437dbba9a4c47cf1e2e7dffc2cfb2e0a65082c6
HamzaAyman <HazmzaAyman1999@Hotmail.com>
2020-10-22 22:20:40 +0300

    new

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(officeSeeder::class);
+        $this->call(order_praductSeeder::class);
+        $this->call(orderSeeder::class);
+        $this->call(employeeSeeder::class);
+        $this->call(customerSeeder::class);
+        $this->call(paymentSeeder::class);
+        $this->call(produtlineSeeder::class);
+        $this->call(produtSeeder::class);
+
     }
 }
